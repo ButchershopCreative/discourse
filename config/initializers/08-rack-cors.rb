ab95c5601b3d453c12ee39610a49cd092210f295
Neil Lalonde <neillalonde@gmail.com>
2014-10-09 17:22:29 -0400

    FEATURE: CORS settings per-site in a multisite env

--- config/initializers/08-rack-cors.rb
+++ config/initializers/08-rack-cors.rb
@@ -1,21 +1,38 @@
-if GlobalSetting.enable_cors && GlobalSetting.cors_origin.present?
-
+if (GlobalSetting.enable_cors && GlobalSetting.cors_origin.present?) || GlobalSetting.enable_multisite_cors
   class Discourse::Cors
     def initialize(app, options = nil)
       @app = app
-      @origins = GlobalSetting.cors_origin.split(',').map(&:strip)
+      if GlobalSetting.enable_cors
+        @global_origins = GlobalSetting.cors_origin.split(',').map(&:strip)
+      end
     end
 
     def call(env)
       status, headers, body = @app.call(env)
       origin = nil
 
-      if origin = env['HTTP_ORIGIN']
-        origin = nil unless @origins.include? origin
+      if @global_origins
+        if origin = env['HTTP_ORIGIN']
+          origin = nil unless @global_origins.include? origin
+        end
+
+        headers['Access-Control-Allow-Origin'] = origin || @global_origins[0]
+        headers['Access-Control-Allow-Credentials'] = "true"
+      else
+        # multisite CORS
+        config = ActiveRecord::Base.connection_pool.spec.config
+        site_origins = config[:cors_origin] ? config[:cors_origin].split(',') : nil
+
+        if site_origins
+          if origin = env['HTTP_ORIGIN']
+            origin = nil unless site_origins.include?(origin)
+          end
+
+          headers['Access-Control-Allow-Origin'] = origin || site_origins.try(:first)
+          headers['Access-Control-Allow-Credentials'] = "true"
+        end
       end
 
-      headers['Access-Control-Allow-Origin'] = origin || @origins[0]
-      headers['Access-Control-Allow-Credentials'] = "true"
       [status,headers,body]
     end
   end
